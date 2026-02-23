be5237f5c4fd8a98ea251ae6c53b33fd2aff9462
Dewmini Gamage <pgdgamage2002@gmail.com>
2026-02-23 21:22:10 +0530

    async

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("Hello NodeJS Lab 04!");
+const condition = true; // change to false to test
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve("Success!");
+  } else {
+    reject("Failure!");
+  }
+});
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
