6a05225342d61a432b001aa969a5d45a2286bc54
Dewmini Gamage <pgdgamage2002@gmail.com>
2026-02-23 21:10:22 +0530

    write

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
   console.log("File saved!");
-fs.readFile("file.txt", "utf8", function (err, data) {
-  if (err) throw err;
-  console.log(data);
+  fs.readFile("file.txt", "utf8", function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
 });
