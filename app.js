5c509a0eca9e463723d7c50629a1b41e4f9d07e3
Dewmini Gamage <pgdgamage2002@gmail.com>
2026-02-23 21:19:29 +0530

    Added Promises example

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-console.log("Hello NodeJS Lab 04!");
-const https = require("https");
-https
-  .get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
-    let data = "";
-    resp.on("data", (chunk) => {
-      data += chunk;
-    });
-    resp.on("end", () => {
-      console.log(JSON.parse(data));
-    });
+const condition = false; // change to false to test failure
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve("Success!");
+  } else {
+    reject("Failure!");
+  }
+});
+
+myPromise
+  .then((result) => {
+    console.log(result);
   })
-  .on("error", (err) => {
-    console.log("Error: " + err.message);
+  .catch((error) => {
+    console.log(error);
   });
